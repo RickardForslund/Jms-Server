84c2b6f252e8ff3fc09f6ac2b78c8199396099d2
toffe <toffe950301@gmail.com>
2021-01-21 11:07:10 +0100

    Added a simple feature to write messages to a text file

--- src/main/java/com/jmsproject/jmsproject/service/RabbitMQListner.java
+++ src/main/java/com/jmsproject/jmsproject/service/RabbitMQListner.java
@@ -4,11 +4,24 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
+
 @Service
-public class RabbitMQListner implements MessageListener {
+public class RabbitMQListner implements MessageListener{
 
     public void onMessage(Message message) {
         System.out.println("Consuming Message - " + new String(message.getBody()));
+        File file = new File("log.txt");
+
+        try {
+            FileWriter fileWriter = new FileWriter(file,true);
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+            writer.write(new String(message.getBody()));
+            writer.newLine();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
